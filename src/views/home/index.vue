2aa9ea0abced4c601fa35653a60774516abffc37
Jaqen <jaqen@iv66.net>
2019-07-11 13:56:29 +0800

    删除首页事例代码

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,6 @@
 <template>
     <div class="home">
-        <div aspectratio w-1-1 class="box">
-            <div aspectratio-content>content_content</div>
-        </div>
-        <div aspectratio w-2-1 class="box">
-            <div aspectratio-content>content</div>
-        </div>
-        <div>
-            <i class="iconfont icon-app"></i>
-        </div>
-        <div class="bg"></div>
-        <div class="icon-header icon-header-one"></div>
-        <div class="icon-footer icon-footer-weibo"></div>
-        <img src="@/assets/img/test.png">
-        <img src="@/assets/img/mini.png" alt="">
+
     </div>
 </template>
 
@@ -23,29 +10,12 @@ export default {
         return{}
     },
     created(){
-        this.$api.test().then(res=>{
-            console.log(res)
-        })
     }
 }
 </script>
 
 
 <style lang="postcss" scoped>
-.box{
-    width: 220px;
-    background-color: #ddd;
-    margin-bottom: 10px;
-}
-.icon-app{
-    font-size: 100px;
-}
-.bg{
-    height: 200px;
-    background-image: url('~@/assets/img/test.png')
-}
-img{
-    width: 200px;
-}
+
 </style>
 
