aa2857b0794d85e60cda27965dddee41367a828f
Jaqen <jaqen@iv66.net>
2019-07-12 11:37:06 +0800

    1px边框问题

--- postcss.config.js
+++ postcss.config.js
@@ -13,6 +13,6 @@ module.exports = {
             minPixelValue: 1,       // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
             mediaQuery: false       // 允许在媒体查询中转换`px`
         },
-        'postcss-viewport-units': {},//必须在postcss-px-to-viewport下面否则无法追加content
+        // 'postcss-viewport-units': {},//必须在postcss-px-to-viewport下面否则无法追加content
     }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="home">
-        <swipe class="home-swipe">
+        <swipe :auto='100000' class="home-swipe">
             <swipe-item v-for="item in banner" :key="item">
                 <img :src="item">
             </swipe-item>
         </swipe>
+        <br/>
+        <div class="setOnePx box"></div>
+        <p>降低呃呃呃 dsf 123</p>
+        <div class="setBorderAll box"></div>
     </div>
 </template>
 
@@ -28,9 +32,44 @@ export default {
 }
 </script>
 
-<style lang="postcss">
+<style lang="postcss" scoped>
     .home-swipe{
         height: 422px;
+    }
+    .box{
+        width: 200px;
+        height: 200px;
+    }
+    .setOnePx{
+        position: relative;
+        &::after{
+            position: absolute;
+            content: '';
+            background-color: red;
+            display: block;
+            width: 100%;
+            height: 1px;
+            transform: scale(1, 0.5);
+            bottom: 0;
+            left: 0;
+            border-radius: 50px;
+        }
+    }
+    .setBorderAll{
+        position: relative;
+        &::after{
+            position:absolute;
+            content:'';
+            top: 0;
+            left: 0;
+            width: 200%;
+            height: 200%;
+            transform: scale(0.5);
+            transform-origin: left top;
+            box-sizing: border-box;
+            border: 1px solid red;
+            border-radius: 50px;
+        }
     }
 </style>
 
