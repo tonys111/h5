fbef832c553d0f8fe82131318810d235b35f680b
korn <korn3323221@gmail.com>
2019-12-28 11:21:26 +0800

    clean

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,6 @@ export default {
         }
     },
     created(){
-        console.log(
-            `%c vue-devtools %c Detected Vue v111232131 %c`,
-            'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
-            'background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
-            'background:transparent'
-        )
     },
     mounted(){
     },
